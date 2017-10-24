5ddeff0fec17e85269a43e64873667e7bd3ffd2a
ANkit <sankit@sankit.com>
2017-10-24 14:03:08 +0530

    Adding model for freezing tutorial

---
+++ Tensorflow-tutorials/freeze_model_and_deploy/download_trained_model.sh
@@ -0,0 +1,4 @@
+wget  	https://s3-ap-southeast-1.amazonaws.com/aimonk/cv-tricks.com/tutorials/FrozenGraph_Sept18/checkpoint
+wget  	https://s3-ap-southeast-1.amazonaws.com/aimonk/cv-tricks.com/tutorials/FrozenGraph_Sept18/dogs-cats-model.data-00000-of-00001
+wget  	https://s3-ap-southeast-1.amazonaws.com/aimonk/cv-tricks.com/tutorials/FrozenGraph_Sept18/dogs-cats-model.index
+wget  	https://s3-ap-southeast-1.amazonaws.com/aimonk/cv-tricks.com/tutorials/FrozenGraph_Sept18/dogs-cats-model.meta
