5670ef68905c0f483b0cbdab41724bd96000296f
ANkit <sankit@sankit.com>
2017-10-25 16:41:39 +0530

    Updating Keras tutorial

--- Tensorflow-tutorials/Keras-Tensorflow-tutorial/3_run_squeezenet.py
+++ Tensorflow-tutorials/Keras-Tensorflow-tutorial/3_run_squeezenet.py
@@ -1,7 +1,7 @@
 import numpy as np
-from keras_squeezenet import SqueezeNet
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from keras.preprocessing import image
+from squeezenet import SqueezeNet
 #import matplotlib.pyplot as plt
 #import matplotlib.image as mpimg
 
