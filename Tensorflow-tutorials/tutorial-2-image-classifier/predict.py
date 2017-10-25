35fd9c98f4d5426f225dd38e67f2b684209ec747
ANkit <sankit@sankit.com>
2017-10-25 16:00:38 +0530

    Updating prediction file

--- Tensorflow-tutorials/tutorial-2-image-classifier/predict.py
+++ Tensorflow-tutorials/tutorial-2-image-classifier/predict.py
@@ -14,7 +14,7 @@ images = []
 # Reading the image using OpenCV
 image = cv2.imread(filename)
 # Resizing the image to our desired size and preprocessing will be done exactly as done during training
-image = cv2.resize(image, (image_size, image_size), cv2.INTER_LINEAR)
+image = cv2.resize(image, (image_size, image_size),0,0, cv2.INTER_LINEAR)
 images.append(image)
 images = np.array(images, dtype=np.uint8)
 images = images.astype('float32')
